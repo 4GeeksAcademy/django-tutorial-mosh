4a75e438e13173304123c8681505bde82a221926
Jackie Vargas <145908828+jackievivianv@users.noreply.github.com>
2024-04-17 23:38:54 +0000

    hasta el debug en vs code

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Request -> Response
+# Request handler
+# Actions
+# In django its called views.
+
+def calculate():
+    x = 1
+    y = 2
+    return x
+
+def say_hello(request):
+#pull data from db
+#send emails
+#transform
+    #return HttpResponse('Hello World')
+    x = calculate()
+    return render(request, 'hello.html', {'name': 'Jackie'})
